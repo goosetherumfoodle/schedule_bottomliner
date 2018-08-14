89aa4f299c839a18370853e58c4bf753fca2f1a0
jesse <goosetherumfoodle@users.noreply.github.com>
2018-08-14 18:42:49 -0400

    changes production log levels

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  Rails.logger = Logger.new(STDOUT)
+  Rails.logger.level = :info
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
